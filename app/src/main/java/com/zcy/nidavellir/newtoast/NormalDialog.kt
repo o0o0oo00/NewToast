ba36a81af6defcbe37b21392ca5d9bdf3a6f4a9e
zhaochunyu <chunyu.zhao@duodian.com>
2018-12-21 14:44:02 +0800

    opt ui

--- app/src/main/java/com/zcy/nidavellir/newtoast/NormalDialog.kt
+++ app/src/main/java/com/zcy/nidavellir/newtoast/NormalDialog.kt
@@ -3,7 +3,6 @@ package com.zcy.nidavellir.newtoast
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.TextView
 
 /**
  * @author:       zhaochunyu
